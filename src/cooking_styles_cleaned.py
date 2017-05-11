4bcc653d5c3f9f1cbb79fed31f5330a06ddf8e49
Megan ORorke <megororke+1@gmail.com>
2017-05-10 17:35:57 -0700

    list of cooking style words

---
+++ src/cooking_styles_cleaned.py
@@ -0,0 +1,18 @@
+
+'''
+Cooking style words
+- List from Wikipedia
+'''
+
+roasting, searing, baking, smoking, grilling (charbroiling),
+rotisserie toasting, blanching, boiling, decoction, parboiling, shocking,
+coddling, creaming, infusion, poaching, simmering, slow cooker, smothering,
+sous-vide, steeping, stewing,
+bain-marie (double boiling), steaming,
+
+hot salt frying, deep frying, pan frying, shallow frying, stir frying (bao), gentle frying, pressure frying,
+
+blackening, browning, reduction, sautéing, sweating,
+* barbecuing, braising, flambé, fricassee, indirect grilling/plank cooking, stir frying (chao),
+air frying, microwaving, pressure cooking, thermal cooking,
+curing, fermenting, pickling, souring,
