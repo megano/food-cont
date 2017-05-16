067bb682ba6172c44ab5e2a51cef9c51e9f23aea
Megan ORorke <megororke+1@gmail.com>
2017-05-15 21:46:59 -0700

    import style csv, drop dup column, lowercase df, extract style word

--- src/food_main.py
+++ src/food_main.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import json
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import datasets, linear_model
+
 
 '''
 Data: Recipe metadata from yummly.com API.
@@ -8,7 +12,7 @@ ID, cuisine, and course (ex: dinner, lunch, salads). Note: data does not
 contain a label for cooking style or the text description of how to make the dish.
 
 Plan:
-Subset the 1 million recipes, identifying ones that have a word in their
+Subset the 1 million recipes, identify ones that have a word in their
 name that matches a cooking style to create a labeled data set. Cooking style
 labels are terms like “blanch” or “roast” from allrecipes.com’s cooking school.
 
@@ -19,17 +23,46 @@ Python, pandas and mongoDB to store and explore data, do feature engineering.
 Model: apply at least 2 supervised learning methods
 (including linear regression, random forest).
 
-TODO:
-- copy source data from other folder
-- make cooking words list
-- set up mongodb
-- set up mongo query pipelines
--- total # recipes
--- # recipes that have 1 or more cooking words
--- # recipes that have no cooking words
--- distribution of recipes per cooking word (need to remove any words?)
--- distribution of cooking words in recipies
 '''
 
-RECIPE_CSV =    'all_recipes_unfiltered.csv'
-RECIPE_CSV_COL_NAMES = ['id', 'name', 'course', 'ingredient']
+# Load the dataset - json
+# with open('style-all.json') as data_file:
+# data = json.load(data_file)
+
+# Load the data set - csv
+style = pd.read_csv('style-all.csv')
+
+# Use only one feature
+style_X = style.data[:, np.newaxis, 2]
+
+# Split the data into training/testing sets
+style_X_train = style_X[:-20]
+style_X_test = style_X[-20:]
+
+# Split the targets into training/testing sets
+style_y_train = style.target[:-20]
+style_y_test = style.target[-20:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(style_X_train, style_y_train)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % np.mean((regr.predict(style_X_test) - style_y_test) ** 2))
+# variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(style_X_test, style_y_test))
+
+# Plot outputs
+plt.scatter(style_X_test, style_y_test,  color='black')
+plt.plot(style_X_test, regr.predict(style_X_test), color='blue',
+         linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
