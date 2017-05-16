913d75f310fcd2dafdf77e27ceddcebde311e363
Megan ORorke <megororke+1@gmail.com>
2017-05-15 21:10:21 -0700

    added query to export data to style-none and style-all csv files

--- src/mongo.py
+++ src/mongo.py
@@ -1,6 +1,12 @@
 '''
 Mongo commands
 
+# import json
+mongoimport --db recipedb --collection recipes --file name.json;
+
+# import csv
+mongoimport --db recipecdb --collection recipes --type csv --headerline --file name.csv;
+
 mongo
 show dbs;
 use recipesDB;
@@ -13,24 +19,31 @@ db.createUser({
 });
 
 # verify total # recipes.
-db.recipes.count()
+db.recipes.count();
 
 # check for match of single style word
-db.recipes.find({ title: "grilled"})
+db.recipes.find({name:{$in:[ /^bake/i]}}).count();
 
 # Count recipes that have any words in the style list in the recipe title.
-db.recipes.find({recipeName:{$in:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}).count();
+db.recipes.find({name:{$in:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}).count();
 
 # count recipes with no style words.
-db.recipes.find({recipeName:{$nin:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}).count();
+db.recipes.find({name:{$nin:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}).count();
 
 # Tested with single word. This query exports records matching "baked" style word into style[stylewordhere].txt
-mongoexport -d recipesDB -c recipes -q '{recipeName:{$in:[ /^bake/i]}}' --out exportdir/style-baked.json
+mongoexport -d recipesDB -c recipes -q '{name:{$in:[ /^bake/i]}}' --out exportdir/style-baked.json
 
 #This query from command line exports all records matching any style word into a file called style-all.json
-mongoexport -d recipesDB -c recipes -q '{recipeName:{$in:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-all.json
+mongoexport -d recipesDB -c recipes -q '{name:{$in:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-all.json
 
 #This query from command line exports all records matching NO style word into a file called style-none.json
-mongoexport -d recipesDB -c recipes -q '{recipeName:{$nin:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-none.json
+mongoexport -d recipesDB -c recipes -q '{name:{$nin:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-none.json
+
+#This query from command line exports all records matching any style word into a file called style-all.csv
+mongoexport -d recipesDB -c recipes --type=csv --fields _id,flavors,rating,totalTimeInSeconds,ingredients,sourceDisplayName,recipeName,attributes,id -q '{recipeName:{$in:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-all.csv
+
+#This query from command line exports all records matching any style word into a file called style-none.csv
+mongoexport -d recipesDB -c recipes --type=csv --fields _id,flavors,rating,totalTimeInSeconds,ingredients,sourceDisplayName,recipeName,attributes,id -q '{recipeName:{$nin:[ /^bake/i, /^scramble/i, /^fried/i, /^boil/i, /^roast/i, /^blanch/i, /^braise/i, /^puree/i, /^poach/i, /^grill/i, /^steam/i, /^mash/i, /^sautee/i, /^gratin/i, /^glaze/i]}}' --out exportdir/style-none.csv
+
 
 '''
